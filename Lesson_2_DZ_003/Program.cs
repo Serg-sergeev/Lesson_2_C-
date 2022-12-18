fc13dc7ab5166118884cde3d97bd0b25e2fabcac
Endnes <sergeev.krut@gmail.com>
2022-12-18 23:32:53 +0300

    Эта программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным

---
+++ Lesson_2_DZ_003/Program.cs
@@ -0,0 +1,15 @@
+﻿string GetWeekend(int number) {
+    if (number == 0 || number == 6) {
+        return "выходной! УРА!!!";
+    }
+    else {
+        return "будний день! Пора пахать!!!";
+    }
+}
+
+int number;
+
+Console.WriteLine("Эта программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+Console.Write("Введите число от 0 до 6: ");
+number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сегодня {GetWeekend(number)}");
