d693ad54784ebe4d19ec2f6af0c2fafb03ef992e
Endnes <sergeev.krut@gmail.com>
2022-12-18 11:29:06 +0300

    Эта программа принимает на вход трехзначное число, а на выходе показывает последнюю цифру

---
+++ Lesson_2_example_001/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetLastDigit(int number){
+    return number % 10;
+}
+
+Console.WriteLine("Эта программа принимает на вход трехзначное число, а на выходе показывает последнюю цифру.");
+Console.Write("Введите трехзначное число ");
+int number = int.Parse(Console.ReadLine()!);
+
+
+
+if (number < 100 || number >= 1000){
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
+else {
+    Console.WriteLine($"Последняя цифра числа {number} - {GetLastDigit(number)}");
+}
