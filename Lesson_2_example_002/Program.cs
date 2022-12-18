c283f8e0bec9f2f4622d858fbd764b49cba8086b
Endnes <sergeev.krut@gmail.com>
2022-12-18 12:31:19 +0300

    Эта программа выводит случайное число из отрезка [10,99] и показывает наибольшею цифру.

---
+++ Lesson_2_example_002/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetMaxDigit(int number) {
+    if (number / 10 > number % 10){
+        return number / 10;
+    }
+    else {
+        return number % 10;
+    }
+}
+
+int number;
+
+Console.WriteLine("Эта программа выводит случайное число из отрезка [10,99] и показывает наибольшею цифру.");
+number = new Random().Next(10,100);
+Console.WriteLine($"Сгенерировано случайное число: {number}");
+Console.WriteLine($"Наибольшая цифра: {GetMaxDigit(number)}");
