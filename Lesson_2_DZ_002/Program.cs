9f06d90a206475630cf029204ca6036ceae5bc7f
Endnes <sergeev.krut@gmail.com>
2022-12-18 20:35:33 +0300

    Эта программа принимает на вход число и показывает тетью цифру этого числа или показывает что третей цифры нет.

---
+++ Lesson_2_DZ_002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Эта программа принимает на вход число и показывает тетью цифру этого числа 
+// или показывает что третей цифры нет.
+
+int GetSecondDigitNumber(int number){
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+
+    return number % 10;
+}
+
+int number;
+
+Console.WriteLine("Эта программа принимает на вход число и показывает тетью цифру этого числа.");
+Console.Write("Введите число: ");
+number = int.Parse(Console.ReadLine()!);
+if (number > 99) {
+    Console.WriteLine($"У числа {number} третья цифра: {GetSecondDigitNumber(number)}");
+}
+else {
+    Console.WriteLine($"У числа {number} нет третьей цифры!!!");
+}
