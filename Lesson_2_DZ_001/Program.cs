9d712e9f10ce2bcd5938fe12bde524b619033e2a
Endnes <sergeev.krut@gmail.com>
2022-12-18 19:53:15 +0300

    Эта программа принимает на вход число и показывает вторую цифру этого числа

---
+++ Lesson_2_DZ_001/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetSecondDigitNumber(int number){
+    while (number > 99)
+    {
+        number = number / 10;
+    }
+
+    return number % 10;
+}
+
+int number;
+
+Console.WriteLine("Эта программа принимает на вход число и показывает вторую цифру этого числа.");
+Console.Write("Введите число: ");
+number = int.Parse(Console.ReadLine()!);
+if (number > 9) {
+    Console.WriteLine($"У числа {number} вторая цифра: {GetSecondDigitNumber(number)}");
+}
+else {
+    Console.WriteLine($"У числа {number} нет второй цифры!!!");
+}
