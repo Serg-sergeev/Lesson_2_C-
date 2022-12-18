fafc40ddadfc00decf448bd18c84958308e7db5b
Endnes <sergeev.krut@gmail.com>
2022-12-18 11:36:16 +0300

    Оптимизация кода

--- Lesson_2_example_001/Program.cs
+++ Lesson_2_example_001/Program.cs
@@ -6,8 +6,6 @@ Console.WriteLine("Эта программа принимает на вход т
 Console.Write("Введите трехзначное число ");
 int number = int.Parse(Console.ReadLine()!);
 
-
-
 if (number < 100 || number >= 1000){
     Console.WriteLine("Вы ввели не трехзначное число!");
 }
